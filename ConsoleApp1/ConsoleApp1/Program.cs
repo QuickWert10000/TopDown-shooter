a5b204bf55047b3bf8c3a0e528fb4d93b2a43083
notalant312 <arturikoks14@gmail.com>
2021-02-09 15:27:41 +0300

    added basics program
    heh)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("shiet post");
+            int _int1 = 7;
+            int _int2 = 341;
+            Console.WriteLine(_int1 *= _int2);
+        }
+    }
+}
